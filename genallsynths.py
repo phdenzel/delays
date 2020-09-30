cde49fa0eee5441d98b9e437b4c871b69ffbee45
phdenzel <phdenzel@gmail.com>
2020-09-30 15:07:32 +0200

    Revision changes

--- genallsynths.py
+++ genallsynths.py
@@ -2,6 +2,7 @@ import sys
 import os
 import pprint
 import numpy as np
+from scipy import ndimage
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 root = "/Users/phdenzel/gleam"
@@ -32,8 +33,8 @@ SYNTH_PLOTS = True
 
 objs = ['B1608+656', 'DESJ0408-5354', 'HE0435-1223', 'PG1115+080',
         'RXJ0911+0551', 'RXJ1131-1231', 'SDSSJ1004+4112', 'WFIJ2033-4723']
-# sigf = [80, 100, 60, 600, 140, 4000, 80, 80]
-sigf = [20, 100, 12, 160, 35, 2000, 2, 140]
+sigf = [80, 100, 60, 600, 140, 4000, 80, 80]
+# sigf = [20, 100, 12, 160, 35, 2000, 2, 140]
 squarify = [0, 0, 0, 0, 0, 0, 0, 0]
 lmaxf = [0.2500, 0.0075, 0.0075, 0.1000, 0.2000, 0.0100, 0.0100, 0.1000]
 smaxf = [1., 10., 10., 0.5, 0.9, .75, 1., 1.]
@@ -52,10 +53,13 @@ print(statefile)
 
 lm = LensModel(statedir+statefile)
 
+lens_idcs = [int(i) for i in sys.argv[1:]] or range(0, 8)
 
 # for objidx in range(0, 8):
 # for objidx in [0, 1, 2, 3, 4, 5, 7]:
-for objidx in [6,]:
+# for objidx in [6,]:
+# for objidx in [4, 5]:
+for objidx in lens_idcs:
     lens = objs[objidx]
     print(lens)
 
@@ -82,8 +86,13 @@ for objidx in [6,]:
     if VIEWSTATE_PLOTS:
         mpl.rcParams['figure.figsize'] = 4, 4
         # Kappa
-        kappa_map_plot(lm, obj_index=objidx, extent=lm.extent, contours=True,
-                       levels=7, delta=0.1, label=lm.obj_name)
+        if 0:  # for 4: RXJ0911
+            flip_extent = list(lm.extent[0:2][::-1]) + list(lm.extent[2:])
+            extnt = flip_extent
+        else:
+            extnt = lm.extent
+        kappa_map_plot(lm, factor=lm.dlsds, obj_index=objidx, extent=extnt,
+                       contours=True, levels=7, delta=0.1, label=lm.obj_name)
         plot_scalebar(R=lm.maprad, length=max(int(lm.maprad/2), 1))
         plt.axis('off')
         plt.gcf().axes[0].get_xaxis().set_visible(False)
@@ -104,8 +113,14 @@ for objidx in [6,]:
         plt.close()
 
         # Arrival-time surface
-        arrival_time_surface_plot(lm, obj_index=objidx, draw_images=True,
-                                  contours=True, levels=60, min_contour_shift=None,
+        if 0:  # for 4: RXJ0911
+            lm.minima = lm.minima * np.array([-1, 1])
+            saddles = lm.saddle_points*1
+            lm.saddle_points = lm.saddle_points * np.array([-1, 1])
+        else:
+            saddles = None
+        arrival_time_surface_plot(lm, geofactor=1./lm.dlsds, psifactor=1., obj_index=objidx, draw_images=True,
+                                  contours=True, levels=60, sad_contour_saddles=saddles, extent=extnt,
                                   sad_contour_shift=None, scalebar=False,
                                   label=lm.obj_name, color='black')
         # plt.plot(0, 0, color=gcl.blue, marker='+', markersize=16)
@@ -126,19 +141,31 @@ for objidx in [6,]:
         lo.data = np.fliplr(lo.data)
     Mo = 80
     if objidx in [5]:
-        Mo = 240
+        Mo = 250
     elif objidx in [6]:
         Mo = 600
-    reconsrc = ReconSrc(lo, lm, M=Mo, M_fullres=400, mask_keys=['circle'])
+    reconsrc = ReconSrc(lo, lm, M=Mo, M_fullres=400, mask_keys=['circle'],
+                        zfactor=lm.dlsds)
     reconsrc.chmdl(-1)
     sig2 = sigf[objidx] * np.abs(reconsrc.lensobject.data)
     sig2[sig2 == 0] = sig2[sig2 != 0].min()
 
     if objidx in [5,]:
+        # reconsrc.rotation = 115.2  # 1*reconsrc.lensobject.hdr['ORIENTAT']
         reconsrc.rotation = -98.2
     if objidx in [9,]:
         reconsrc.rotation = 1*reconsrc.lensobject.hdr['ORIENTAT']
 
+    # dij = reconsrc.lens_map(mask=True)
+    # plt.imshow(dij, extent=reconsrc.extent, origin='lower',
+    #            vmin=0, vmax=lmaxf[objidx]*np.max(dij), interpolation='bicubic',
+    #            cmap='gravic')
+    # phcmap = plt.get_cmap('phoenix')
+    # min_clr, sad_clr = phcmap(.2), phcmap(.5)
+    # plt.plot(lm.minima.T[0], lm.minima.T[1], color=min_clr, marker='o', lw=0)
+    # plt.plot(lm.saddle_points.T[0], lm.saddle_points.T[1], color=sad_clr, marker='o', lw=0)
+    # plt.show()
+    # exit(1)
 
     # PSF
     if objidx in [0, 1, 2, 7]:
@@ -156,7 +183,7 @@ for objidx in [6,]:
 
     # Calculating lens and source plane
     kw = dict(method='minres', use_psf=True, use_mask=True,
-              use_filter=False, sigma2=sig2.copy(), cached=True)
+              use_filter=False, sigma2=sig2.copy(), cached=False)
     wrad = 0.8
     if objidx in [3, 4, 7]:
         wrad = 0.5
@@ -181,9 +208,14 @@ for objidx in [6,]:
                                 within_radius=wrad, from_cache=False,
                                 save_to_cache=False, **kw)
     print("Chi2: {}".format(chi2))
-    if objidx in [4,]:
+    if objidx in [4, 5]:
         dij = np.fliplr(dij)
         synth = np.fliplr(synth)
+        residmap = np.fliplr(residmap)
+        reconsrc.rotation = 115.2 - 98.2
+        dij = ndimage.rotate(dij, reconsrc.rotation, reshape=False)
+        synth = ndimage.rotate(synth, reconsrc.rotation, reshape=False)
+        residmap = ndimage.rotate(residmap, reconsrc.rotation, reshape=False)
 
     # Plotting
     lmax = lmaxf[objidx] * np.max(dij)
